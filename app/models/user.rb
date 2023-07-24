579a6c1963b4d33061f535f930e7f618acd30e5b
WakeelMustafa <wakeelmustafa999@gmail.com>
2023-07-24 13:20:02 +0500

    Initial Commit

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+
+  has_one_attached:avatar
+  attribute :firstname
+  attribute :lastname
+  before_save :concatenate_fullname
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+
+  private
+
+  def concatenate_fullname
+    self.fullname = "#{firstname} #{lastname}".strip
+  end
+
+end
