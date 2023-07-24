9233bac007e8ae4deecde0c4e1a4e6f188f2beca
WakeelMustafa <wakeelmustafa999@gmail.com>
2023-07-24 14:47:03 +0500

    Creating Product Model and develop associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-
+  has_many :products
   has_one_attached:avatar
   attribute :firstname
   attribute :lastname
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'users/registrations' }
 
   root to: "products#index"
-  resources:products
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resource :users do
+end
+  resources:products
 end
