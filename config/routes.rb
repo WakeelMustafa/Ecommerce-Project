7935b64e68d566f99d7a15fef893c0877dccf665
WakeelMustafa <wakeelmustafa999@gmail.com>
2023-07-25 11:20:18 +0500

    Creating Comment Controller and Model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'comments/index'
+  get 'comments/show'
+  get 'comments/edit'
+  get 'comments/new'
   devise_for :users, :controllers => { registrations: 'users/registrations' }
   root to: "myproducts#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
